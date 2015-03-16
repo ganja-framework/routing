40d6274a479414db2e7f671a1ec405a76dbf530f
Janusz Slota <janusz.slota@allsaints.com>
2015-03-16 18:01:08 +0000

    More tests

--- test/ganja/component/routing/utils/PatternSpec.groovy
+++ test/ganja/component/routing/utils/PatternSpec.groovy
@@ -24,15 +24,21 @@ class PatternSpec extends Specification {
         java.util.regex.Pattern.compile(pattern).matcher(input).matches()
 
         where:
-        input                   | pattern
-        '/foo'                  | '^/(?<foo>[^/]+)$'
-        '/bar'                  | '^/(?<foo>[^/]+)$'
-        '/1345'                 | '^/(?<foo>[^/]+)$'
-        '/foo'                  | '^/(?<foo>[^/]+)(?:/(?<bar>[^/]+))?$'
-        '/foo/bar'              | '^/(?<foo>[^/]+)(?:/(?<bar>[^/]+))?$'
-        '/admin/pages/134'      | '^/admin/pages/(?<foo>[^/]+)$'
-        '/admin/pages/134/edit' | '^/admin/pages/(?<foo>[^/]+)(?:/(?<bar>[^/]+))?$'
-        '/cms/pages/134/edit'   | '^/(?<module>[^/]+)/pages/(?<pageId>[^/]+)/(?<action>[^/]+)$'
-        '/foo/231'              | '^/foo/(?<pageId>[^/]+)$'
+        input                     | pattern
+        '/foo'                    | '^/(?<foo>[^/]+)$'
+        '/bar'                    | '^/(?<foo>[^/]+)$'
+        '/1345'                   | '^/(?<foo>[^/]+)$'
+        '/foo'                    | '^/(?<foo>[^/]+)(?:/(?<bar>[^/]+))?$'
+        '/foo/bar'                | '^/(?<foo>[^/]+)(?:/(?<bar>[^/]+))?$'
+        '/'                       | '^/(?:(?<foo>[^/]+))?(?:/(?<bar>[^/]+))?(?:/(?<id>[^/]+))?$'
+        '/foo'                    | '^/(?:(?<foo>[^/]+))?(?:/(?<bar>[^/]+))?(?:/(?<id>[^/]+))?$'
+        '/1236'                   | '^/(?:(?<foo>[^/]+))?(?:/(?<bar>[^/]+))?(?:/(?<id>[^/]+))?$'
+        '/foo/bar'                | '^/(?:(?<foo>[^/]+))?(?:/(?<bar>[^/]+))?(?:/(?<id>[^/]+))?$'
+        '/foo/bar/1236'           | '^/(?:(?<foo>[^/]+))?(?:/(?<bar>[^/]+))?(?:/(?<id>[^/]+))?$'
+        '/admin/pages/134'        | '^/admin/pages/(?<foo>[^/]+)$'
+        '/admin/pages/acb234a452' | '^/admin/pages/(?<foo>[^/]+)$'
+        '/admin/pages/134/edit'   | '^/admin/pages/(?<foo>[^/]+)(?:/(?<bar>[^/]+))?$'
+        '/cms/pages/134/edit'     | '^/(?<module>[^/]+)/pages/(?<pageId>[^/]+)/(?<action>[^/]+)$'
+        '/foo/231'                | '^/foo/(?<pageId>[^/]+)$'
     }
 }
